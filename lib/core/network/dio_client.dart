abdbe8fef3023e2e935f441c41f08f50ad1f94cd
BryanIanRamos <bryanramos3367@gmail.com>
2026-07-20 10:06:57 +0800

    sadasdas

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -8,11 +8,11 @@ class DioClient {
     dio = Dio(
       BaseOptions(
         baseUrl: EnvConfig.basedUrl,
-        connectTimeout: const Duration(seconds: 10),
-        receiveTimeout: const Duration(seconds: 10),
+        connectTimeout: Duration(seconds: 10),
+        receiveTimeout: Duration(seconds: 10),
 
         headers: {
-          "Accept": 'application/json',
+          'accept': 'application/json',
           'Content-Type': 'application/json',
         },
       ),
