e50759077f54f3a01ab85af00e09bce4bad167a6
BryanIanRamos <bryanramos3367@gmail.com>
2026-07-30 09:30:34 +0800

    feat: get http call completed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,27 +16,28 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home Screenss")),
+      appBar: AppBar(title: Text("HomeScreen")),
       floatingActionButton: FloatingActionButton(
-        onPressed: fetchUser,
-        child: Icon(Icons.access_alarm_outlined),
+        onPressed: fetchData,
+        child: Icon(Icons.add),
       ),
       body: ListView.builder(
         itemCount: users.length,
         itemBuilder: (context, index) {
           final user = users[index];
+          final name = user['name']['first'];
           final email = user['email'];
-
           return ListTile(
-            leading: CircleAvatar(child: Text("${index + 1}")),
-            title: Text("$email"),
+            leading: CircleAvatar(child: Text("$index")),
+            title: Text("$name"),
+            subtitle: Text("$email"),
           );
         },
       ),
     );
   }
 
-  Future<void> fetchUser() async {
+  Future<void> fetchData() async {
     try {
       const url = 'https://randomuser.me/api/?results=10';
       final response = await http.get(Uri.parse(url));
@@ -47,11 +48,13 @@ class _HomeScreenState extends State<HomeScreen> {
         setState(() {
           users = json['results'];
         });
+
+        print("Fetched Data: $users");
       } else {
-        debugPrint('HTTP Error: ${response.statusCode}');
+        debugPrint("Http Error: ${response.statusCode}");
       }
     } catch (e) {
-      debugPrint('Network Error: $e');
+      debugPrint("Network Error: $e");
     }
   }
 }
